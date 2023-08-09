16e514268dcd011cf7bc1a32cfb180acf6ffcd77
Johan Eliasson <johan@eliasson.me>
2023-08-09 14:08:48 +0200

    update

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -9,6 +9,16 @@ export default function App({ Component, pageProps }: AppProps) {
       <DefaultSeo
         title="Nyheter.sh: Swedish news in English."
         description="Explore Nyheter.sh for the latest Swedish news in English. Stay updated on politics, sports, entertainment, and more from Sweden's diverse regions. Translate the world of Swedish events and culture with daily updates and insights, all available at your fingertips at Nyheter.sh. Join us now for a global perspective on local stories."
+        openGraph={{
+          images: [
+            {
+              url: 'https://nyheter.sh/og.png',
+              alt: 'Nyheter.sh: Swedish news in English.',
+              type: 'image/png',
+            },
+          ],
+          siteName: 'Nyheter.sh',
+        }}
         twitter={{
           handle: '@elitasson',
           site: '@nyheter.sh',
