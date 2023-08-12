b049a1b41ed310e14a912186cbde091cd367c32b
Johan Eliasson <johan@eliasson.me>
2023-08-13 00:00:30 +0200

    fix

--- web/src/components/Layout.tsx
+++ web/src/components/Layout.tsx
@@ -28,11 +28,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
 
   return (
     <>
-      <TopBanner />
+      {/* <TopBanner /> */}
       <div className="bg-slate-200">
         <div className="bg-white">
           <MainContainer>
-            <div className="flex flex-col md:flex-row space-y-4 text-center md:text-left justify-between py-8 items-center">
+            <div className="flex flex-col md:flex-row space-y-4 text-center md:text-left justify-between py-4 items-center">
               <Link className="space-x-4 flex items-center" href="/">
                 <div>
                   <img src="/logo.png" className="h-10 w-10 " alt="Logo" />
