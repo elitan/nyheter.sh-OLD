0d27a7faf90b1942adc66b2308efcc15d0d5a654
Johan Eliasson <johan@eliasson.me>
2023-08-15 23:58:57 +0200

    asd

--- worker/run.sh
+++ worker/run.sh
@@ -6,8 +6,18 @@ while true; do
     pnpm openai
     pnpm su
     pnpm elevenlabs
+
     for (( i=1200; i>=0; i-- )); do
         echo "$i"
-        sleep 1
+        # Try to read a key press with a timeout of 1 second
+        read -t 1 -n 1 input
+        if [[ $input = "" ]]; then
+            continue
+        else
+            # If Enter is pressed, break out of the countdown loop
+            if [[ $input = $'\n' ]]; then
+                break
+            fi
+        fi
     done
 done
