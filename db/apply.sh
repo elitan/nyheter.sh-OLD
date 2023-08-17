952451d51e61cca6334e8bd47f545d919526ac0b
Johan Eliasson <johan@eliasson.me>
2023-08-17 20:56:44 +0200

    asd

--- db/apply.sh
+++ db/apply.sh
@@ -4,6 +4,6 @@
 #   --dev-url "docker://postgres/15/test" \
 
 atlas schema apply \
-  -u "postgres://elitan:nI2UokMqE8Wa@ep-fragrant-dew-970345.eu-central-1.aws.neon.tech/srai?sslmode=require" \
+  -u "postgres://postgres:password@ai:9001/postgres?sslmode=disable" \
   --to file://schema.sql \
   --dev-url "docker://postgres/15/test" \
