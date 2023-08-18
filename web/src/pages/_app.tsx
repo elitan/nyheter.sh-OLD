d402f83cdf02579e4fe82c5f0205aa28188fdd74
Johan Eliasson <johan@eliasson.me>
2023-08-18 18:16:26 +0200

    analytics

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -2,32 +2,38 @@ import { Layout } from '@/components/Layout';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { DefaultSeo } from 'next-seo';
-import { GoogleAnalytics } from 'nextjs-google-analytics';
+import PlausibleProvider from 'next-plausible';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <GoogleAnalytics trackPageViews />
-      <DefaultSeo
-        title="Swedish news in English - Nyheter.sh"
-        description="Explore Nyheter.sh for the latest Swedish news in English. Stay updated on politics, sports, entertainment, and more from Sweden's diverse regions. Translate the world of Swedish events and culture with daily updates and insights, all available at your fingertips at Nyheter.sh. Join us now for a global perspective on local stories."
-        openGraph={{
-          images: [
-            {
-              url: 'https://nyheter.sh/og.png',
-              alt: 'Nyheter.sh: Swedish news in English.',
-              type: 'image/png',
-            },
-          ],
-          siteName: 'Nyheter.sh',
-        }}
-        twitter={{
-          handle: '@elitasson',
-          site: '@nyheter.sh',
-          cardType: 'summary_large_image',
-        }}
-      />
-      <Component {...pageProps} />
-    </Layout>
+    <PlausibleProvider
+      domain="nyheter.sh"
+      customDomain="https://analytics.eliasson.me"
+      selfHosted={true}
+      enabled={true}
+    >
+      <Layout>
+        <DefaultSeo
+          title="Swedish news in English - Nyheter.sh"
+          description="Explore Nyheter.sh for the latest Swedish news in English. Stay updated on politics, sports, entertainment, and more from Sweden's diverse regions. Translate the world of Swedish events and culture with daily updates and insights, all available at your fingertips at Nyheter.sh. Join us now for a global perspective on local stories."
+          openGraph={{
+            images: [
+              {
+                url: 'https://nyheter.sh/og.png',
+                alt: 'Nyheter.sh: Swedish news in English.',
+                type: 'image/png',
+              },
+            ],
+            siteName: 'Nyheter.sh',
+          }}
+          twitter={{
+            handle: '@elitasson',
+            site: '@nyheter.sh',
+            cardType: 'summary_large_image',
+          }}
+        />
+        <Component {...pageProps} />
+      </Layout>
+    </PlausibleProvider>
   );
 }
