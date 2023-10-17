452d16768e3b24c22b1955c0c8f895fbe7d0cf43
Johan Eliasson <johan@eliasson.me>
2023-10-18 00:09:23 +0200

    added  test comment

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -9,6 +9,8 @@ import { api } from '@/utils/api';
 import 'react-toastify/dist/ReactToastify.min.css';
 import { ToastContainer } from 'react-toastify';
 
+// test
+
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <PlausibleProvider
